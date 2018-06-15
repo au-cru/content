922bf9c5f07b718ba83e77efa6ada0628b1c891b
Luke W Johnston <lwjohnst@gmail.com>
2018-06-16 00:08:33 +0200

    add video link and format date functions

--- R/utils.R
+++ R/utils.R
@@ -1,2 +1,17 @@
 
-knitr::opts_chunk$set(comment = "#>", fig.path = "images")
+knitr::opts_chunk$set(comment = "#>", fig.path = "images/")
+
+add_video_link <- function(video_param) {
+    if (!is.null(video_param))
+        paste0("- **Session [video recording](", video_param, ")**")
+}
+
+# embed_video <- function(video_param) {
+#     url <- ""
+#
+#     <iframe width="560" height="315" src="https://www.youtube.com/embed/tPSSNF3yado" frameborder="0" allow="autoplay; encrypted-media" allowfullscreen></iframe>
+# }
+
+format_date <- function(date_param) {
+    format(as.Date(date_param), format = "%d %b, %Y")
+}
